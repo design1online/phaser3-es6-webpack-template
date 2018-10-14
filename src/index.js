3166cb2b816b1eb89c550da0fd845afdd5747dc0
Greta Krafsig <greta.krafsig@washpost.com>
2018-10-14 14:43:52 -0400

    Fixing linter errors

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import 'phaser';
+import Phaser from 'phaser';
 import TweenExample from './examples/animations/tween';
 
 const config = {
-    // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
-    type: Phaser.WEBGL,
-    pixelArt: true,
-    roundPixels: true,
-    parent: 'content',
-    width: 800,
-    height: 600,
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 800 },
-        debug: false
-      }
+  // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+  type: Phaser.WEBGL,
+  pixelArt: true,
+  roundPixels: true,
+  parent: 'content',
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 800 },
+      debug: false,
     },
-    scene: [
-      TweenExample,
-    ]
+  },
+  scene: [
+    TweenExample,
+  ],
 };
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config); // eslint-disable-line no-unused-vars
