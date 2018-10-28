49b2fee3095d4f9761f7d774ccc5ac33eef1f10a
Jade Krafsig <jade@design1online.com>
2018-10-28 16:35:36 -0400

    Fixing linter issues

--- src/config/default.js
+++ src/config/default.js
@@ -9,24 +9,38 @@ import { AnimationData, AnimationTween, AnimationAdd } from '../examples/animati
 /**
 * @desc Default Phaser configuration, for more settings see {@link https://photonstorm.github.io/phaser3-docs/global.html#GameConfig}
 * @typedef {Object} defaultConfig
-* @property {number} [type=Phaser.AUTO] Phaser.AUTO, Phaser.CANVAS, Phaser.HEADLESS or Phaser.WEBGL. AUTO picks WEBGL if available, otherwise CANVAS.
+* @property {number} [type=Phaser.AUTO] Phaser.AUTO, Phaser.CANVAS, Phaser.HEADLESS or Phaser.WEBGL.
+* AUTO picks WEBGL if available, otherwise CANVAS.
 * @property {number|string} [width=1024] The width of the game, in game pixels
 * @property {number|string} [height=768] The height of the game, in game pixels
-* @property {number} [zoom=1] Simple scale applied to the game canvas. 2 is double size, 0.5 is half size, etc
-* @property {number} [resolution=1] The size of each game pixel, in canvas pixels. Values larger than 1 are "high" resolution
-* @property {Object|string} [parent=null] The DOM element that will contain the game canvas, or its id. If null (the default) or if the named element doesn't exist, the game canvas is inserted directly into the document body
-* @property {Object} [canvas=null] Provide your own Canvas element for Phaser to use instead of creating one.
-* @property {string} [canvasStyle=null] CSS styles to apply to the game canvas instead of Phaser's default styles.
-* @property {Object} [CanvasRenderingContext2D] Provide your own Canvas Context for Phaser to use, instead of creating one.
-* @property {Object} [scene=null] A scene or scenes to add to the game. If several are given, the first is started; the remainder are started only if they have { active: true }.
-* @property {string[]} [seed]	Seed for the random number generator
+* @property {number} [zoom=1] Simple scale applied to the game canvas. 2 is double size,
+*  0.5 is half size, etc
+* @property {number} [resolution=1] The size of each game pixel, in canvas pixels. Values
+*  larger than 1 are "high"
+* resolution
+* @property {Object|string} [parent=null] The DOM element that will contain the game canvas,
+* or its id. If null (the default) or if the named element doesn't exist, the game canvas is
+* inserted directly into the document body
+* @property {Object} [canvas=null] Provide your own Canvas element for Phaser to use instead of
+* creating one.
+* @property {string} [canvasStyle=null] CSS styles to apply to the game canvas instead of Phaser's
+*  default styles.
+* @property {Object} [CanvasRenderingContext2D] Provide your own Canvas Context for Phaser to use,
+* instead of creating one.
+* @property {Object} [scene=null] A scene or scenes to add to the game. If several are given,
+* the first is started; the remainder are started only if they have { active: true }.
+* @property {string[]} [seed] Seed for the random number generator
 * @property {string} [title=''] The title of the game shown in the browser console
 * @property {string} [url='http://phaser.io'] The URL of the game shown in the browser console
 * @property {string} [version=''] The version of the game shown in the browser console
-* @property {boolean} [autoFocus=true] Automatically call window.focus() when the game boots. Usually necessary to capture input events if the game is in a separate frame
-* @property {boolean|InputConfig} [input] Input configuration, or false to disable all game input. See {@link https://photonstorm.github.io/phaser3-docs/global.html#InputConfig}
-* @property {boolean} [disableContextMenu=false] Disable the browser's default 'contextmenu' event (usually triggered by a right-button mouse click)
-* @property {boolean|BannerConfig} [banner=false] Configuration for the banner printed in the browser console when the game starts. See {@link https://photonstorm.github.io/phaser3-docs/global.html#BannerConfig}
+* @property {boolean} [autoFocus=true] Automatically call window.focus() when the game boots.
+* Usually necessary to capture input events if the game is in a separate frame
+* @property {boolean|InputConfig} [input] Input configuration, or false to disable all game input.
+* See {@link https://photonstorm.github.io/phaser3-docs/global.html#InputConfig}
+* @property {boolean} [disableContextMenu=false] Disable the browser's default 'contextmenu' event
+* (usually triggered by a right-button mouse click)
+* @property {boolean|BannerConfig} [banner=false] Configuration for the banner printed in the
+* browser console when the game starts. See {@link https://photonstorm.github.io/phaser3-docs/global.html#BannerConfig}
 * @property {DOMContainerConfig} [dom] The DOM Container configuration object, see {@link https://photonstorm.github.io/phaser3-docs/global.html#DOMContainerConfig}
 * @property {FPSConfig} [fps] Game loop configuration, see {@link https://photonstorm.github.io/phaser3-docs/global.html#FPSConfig}
 * @property {RenderConfig} [render] Game render configuration, see {@link https://photonstorm.github.io/phaser3-docs/global.html#RenderConfig}
